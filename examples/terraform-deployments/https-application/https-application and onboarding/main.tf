2148084e71c638bdae7df91bda17cd4c031179da
karce25 <123678611+karce25@users.noreply.github.com>
2023-05-11 10:50:49 -0600

    Update main.tf

--- examples/terraform-deployments/https-application/https-application and onboarding/main.tf
+++ examples/terraform-deployments/https-application/https-application and onboarding/main.tf
@@ -5,7 +5,7 @@ resource "bigip_do" "do-example" {
 }
 
 resource "bigip_as3" "as3-example1" {
-  as3_json = file("example.json")
+  as3_json = file("https_app_as3.json")
   depends_on = [bigip_do.do-example ]
 }
 
